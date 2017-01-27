0772574f4b4bc27ab518fcfde9d20101018a1573
kujirahand <web@kujirahand.com>
2017-01-27 14:06:49 +0900

    add README

--- 3.1/setup.sh
+++ 3.1/setup.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
+# ---
+# Raspberry Pi (Jessie) に
+#   OpenCV 3.1.0をインストールする
+# ---
 
-sudo apt-get install -y cmake python3-dev python3.2-dev python3-numpy gcc gcc-c++ build-essential cmake-curses-gui
+# 必要なライブラリをインストール
+sudo apt-get install -y cmake python3-dev python3.2-dev python3-numpy gcc build-essential cmake-curses-gui
 
 sudo apt-get install -y pkg-config libpng12-0 libpng12-dev libpng++-dev libpng3 libpnglite-dev zlib1g-dbg zlib1g zlib1g-dev pngtools libtiff5-dev libtiff4 libtiffxx0c2 libtiff-tools libeigen3-dev
 
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk2.0-dev libatlas-base-dev gfortran wget
 
+# 必要なZIPファイルをダウンロード
 cd ~/
 
 wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
 wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
+# ZIPを解凍
 unzip opencv.zip
 unzip opencv_contrib.zip
 
+# ビルド
 cd ~/opencv-3.1.0/
-
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
