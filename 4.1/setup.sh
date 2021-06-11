194a86880622148e80ec1fc4e354a0a18dbd70d6
kujirahand <kujira@kujirahand.com>
2021-06-11 23:17:05 +0900

    add description

--- 4.1/setup.sh
+++ 4.1/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo "--- update ---"
 sudo apt update
 sudo apt upgrade
 
+echo "--- apt install library ---"
 sudo apt install -y libqt4-test libqtgui4 libjasper1 libatlas-base-dev libhdf5-dev
 
+echo "--- pip3 install opencv-python ---"
 sudo pip3 install opencv-python==4.1.0.25
 sudo pip3 install opencv-contrib-python==4.1.0.25
 
